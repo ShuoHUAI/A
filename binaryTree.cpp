f680fcdb1baa97a64e5de3caa26bd24c854d9a66
ShuoHUAI <17853143761@163.com>
2016-12-04 14:09:21 +0800

    binaryTree.cpp

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -38,36 +38,5 @@ void binaryTree<T>::levelOrder(listNode<T> *t){
         q->pop();
     }
 }
-int main(){
-	binaryTree<int> *one = new binaryTree<int>();
-    int aa = 2;
-    listNode<int> *a = new listNode<int>(aa);
-    one->root = a;
-    aa = 5;
-    a = new listNode<int> (aa);
-    one->root->leftChild=a;
-    aa = 3;
-    a = new listNode<int> (aa);
-    one->root->rightChild=a;
-    aa = 4;
-    a = new listNode<int>(aa);
-    one->root->leftChild->leftChild=a;
-    aa = 7;
-    a = new listNode<int> (aa);
-    one->root->rightChild->leftChild=a;
-    aa = 8;
-    a = new listNode<int> (aa);
-    one->root->rightChild->rightChild=a;
-    cout<<endl<<"PreOrder..."<<endl;
-    one->preOrder(one->root);
-    cout<<endl<<"InOrder..."<<endl;
-    one->inOrder(one->root);
-    cout<<endl<<"PostOoder..."<<endl;
-    one->postOrder(one->root);
-    cout<<endl<<"LevelOrder..."<<endl;
-    one->levelOrder(one->root);
-    cout<<endl<<"The number of node "<<one->size(one->root)<<endl<<"The height of the binarytree "<<one->height(one->root)<<endl;
-    //cout<<one->root->leftChild;*/
-	return 0;
-}
+
 
